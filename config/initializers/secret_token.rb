1ae75348c164626cc54f1ad608ad48d458c0efb9
Bryan Chang <keunyoung.chang@gmail.com>
2013-04-21 14:32:47 -0700

    First commit - devise installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Younified::Application.config.secret_token = 'bb45baba97ca24de07da7b9381947902d004a99b55124796902b8a3134b71ca0a15ad845049986908ca4abe31cf0d840d1c8997e0c33efe92d0f1cf547f678db'
